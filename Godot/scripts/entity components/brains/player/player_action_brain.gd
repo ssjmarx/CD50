70449f3803f1bd6e7280d26485cd5818a50310d2
Your Name <your.email@example.com>
2026-06-19 18:12:34 -0700

    refactor: manage input connections in wake/sleep cycle

--- Godot/scripts/entity components/brains/player/player_action_brain.gd
+++ Godot/scripts/entity components/brains/player/player_action_brain.gd
@@ -14,8 +14,7 @@ func _ready() -> void:
 
 ## on initialize
 func _on_initialize() -> void:
-	game.input_router.input_action_pressed.connect(_on_action_pressed)
-	game.input_router.input_action_released.connect(_on_action_released)
+	wake()
 
 ## on action pressed
 func _on_action_pressed(pid: int, action: StringName) -> void:
@@ -28,6 +27,24 @@ func _on_action_released(pid: int, action: StringName) -> void:
 	if pid != player_id or action not in action_mappings:
 		return
 	entity.bus_emit(StringName(action + &"_end"))
+
+## on sleep                                                               
+func _on_sleep() -> void:                                                 
+	super._on_sleep()                                                     
+	if is_instance_valid(game) and game.input_router:                     
+		if game.input_router.input_action_pressed.is_connected(_on_action_pressed):  
+			game.input_router.input_action_pressed.disconnect(_on_action_pressed)                                                                   
+		if game.input_router.input_action_released.is_connected(_on_action_released):
+			game.input_router.input_action_released.disconnect(_on_action_released)                                                                 
+																		  
+## on wake                                                                
+func _on_wake() -> void:                                                  
+	super._on_wake()                                                      
+	if is_instance_valid(game) and game.input_router:                     
+		if not game.input_router.input_action_pressed.is_connected(_on_action_pressed):  
+			game.input_router.input_action_pressed.connect(_on_action_pressed)                                                                      
+		if not game.input_router.input_action_released.is_connected(_on_action_released):
+			game.input_router.input_action_released.connect(_on_action_released)   
 
 ## on entity deactivating
 func _on_entity_deactivating() -> void:
